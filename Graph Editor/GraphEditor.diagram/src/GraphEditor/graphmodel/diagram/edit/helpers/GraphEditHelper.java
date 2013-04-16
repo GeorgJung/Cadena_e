c2fabbc886229fa53acc6c622d82db27d115f16d
omarelalfy <omar.el1000@gmail.com>
2013-04-16 19:10:35 +0200

    Updated Graph Editor

--- Graph Editor/GraphEditor.diagram/src/GraphEditor/graphmodel/diagram/edit/helpers/GraphEditHelper.java
+++
@@ -1,8 +0,0 @@
-package GraphEditor.graphmodel.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class GraphEditHelper extends
-		GraphEditor.graphmodel.diagram.edit.helpers.GraphModelBaseEditHelper {
-}
--- Graph Editor/GraphEditor.diagram/src/GraphEditor/graphmodel/diagram/edit/helpers/NodeEditHelper.java
+++
@@ -1,8 +0,0 @@
-package GraphEditor.graphmodel.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class NodeEditHelper extends
-		GraphEditor.graphmodel.diagram.edit.helpers.GraphModelBaseEditHelper {
-}
